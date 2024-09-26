bdca871a4ef52184355a95e45ccb1d594ceca10b
Squamto <25984337+Squamto@users.noreply.github.com>
2024-09-26 17:14:35 +0200

    Kernel Dev

--- lua/commands.lua
+++ lua/commands.lua
@@ -5,3 +5,15 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  desc = "Coding Style for Kernel",
+  group = vim.api.nvim_create_augroup('project-configs', { clear = false }),
+  pattern = { "*kernel*/*.c", "*kernel*/*.h" },
+  callback = function()
+    vim.bo.expandtab = false
+    vim.bo.shiftwidth = 8
+    vim.bo.softtabstop = 8
+    vim.bo.tabstop = 8
+  end,
+})
