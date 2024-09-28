d9c32a56459536874c07f7814c3c06a1f56367c7
Squamto <25984337+Squamto@users.noreply.github.com>
2024-09-28 11:20:00 +0200

    kernel Check

--- lua/commands.lua
+++ lua/commands.lua
@@ -15,5 +15,68 @@ vim.api.nvim_create_autocmd("BufEnter", {
     vim.bo.shiftwidth = 8
     vim.bo.softtabstop = 8
     vim.bo.tabstop = 8
+
+    local namespace = vim.api.nvim_create_namespace('checkpath')
+
+    local function run_checkpatch()
+      local filename = vim.api.nvim_buf_get_name(0)
+      local cmd = 'scripts/checkpatch.pl --no-tree --no-signoff --terse -f ' .. filename
+      local output = vim.fn.system(cmd)
+
+      if vim.api.nvim_get_vvar('shell_error') == 127 then
+        vim.api.nvim_err_writeln('Error running checkpatch: ' .. output)
+        return
+      end
+
+      if output == '' then
+        print('No issues found')
+        return
+      end
+
+      local lines = vim.split(output, '\n')
+      ---@type vim.Diagnostic[]
+      local diagnostics = {}
+
+      for _, line in ipairs(lines) do
+        local parts = vim.split(line, ':')
+        if #parts < 3 then
+          goto continue
+        end
+
+        -- if filename ~= parts[1] then
+        --   vim.api.nvim_err_writeln('filename does not match')
+        -- end
+        local linenum = tonumber(parts[2])
+        local type = parts[3]
+        local message = parts[4]
+        local severity = vim.diagnostic.severity.INFO
+
+        if type == 'ERROR' then
+          severity = vim.diagnostic.severity.ERROR
+        elseif type == 'WARNING' then
+          severity = vim.diagnostic.severity.WARN
+        end
+
+        if linenum == nil or type == nil or message == nil then
+          goto continue
+        end
+
+        diagnostics[#diagnostics + 1] = {
+          source = 'checkpatch',
+          lnum = linenum,
+          col = 0,
+          message = message,
+          severity = severity,
+        }
+
+        ::continue::
+      end
+
+      vim.diagnostic.set(namespace, vim.api.nvim_get_current_buf(), diagnostics)
+    end
+
+    vim.keymap.set('n', '<leader>kc', function()
+      run_checkpatch()
+    end, { silent = true, desc = 'Run checkpatch' })
   end,
 })
--- lua/custom/plugins/undotree.lua
+++ lua/custom/plugins/undotree.lua
@@ -1,3 +1,9 @@
 return {
-  'mbbill/undotree'
+  {
+    'mbbill/undotree',
+    keys = '<leader>u',
+    config = function()
+      vim.keymap.set('n', '<leader>u', ':UndotreeToggle<CR>', { silent = true, desc = 'Toggle UndoTree' })
+    end
+  }
 }
