413c7f806a42b9db47210cc96f28c5ddb09c0f63
Squamto <25984337+Squamto@users.noreply.github.com>
2024-11-24 13:14:42 +0100

    Disable Copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -2,5 +2,11 @@ return {
   {
     'github/copilot.vim',
     cond = vim.fn.executable 'node' == 1,
-  }
+    lazy = true,
+    cmd = 'Copilot',
+    -- disable by default
+    config = function()
+      vim.g['copilot#enable'] = 0
+    end,
+  },
 }
