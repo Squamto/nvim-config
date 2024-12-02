108ae746b32cc24f9b0d5dd37e60edec6f8eec4b
Squamto <25984337+Squamto@users.noreply.github.com>
2024-12-02 23:33:23 +0100

    Wrapped Navigation

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,9 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open float
 vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist, { desc = 'Push Buffer [D]iagnostics to [Q]uickFixList' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setqflist, { desc = 'Push All [D]iagnostics to [Q]uickFixList' })
 
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
+
 vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to the window on the left' })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to the window below' })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to the window above' })
